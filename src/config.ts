4d50eeb27d75e618c04904b0088bd086f590eb30
Lukmanyyy <lukmanardiansyah46@gmail.com>
2026-08-08 19:51:21 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 export const CONFIG = {
   // 🎵 GANTI LINK LAGU DI SINI (BISA PAKAI LINK GOOGLE DRIVE)
-  AUDIO_URL: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+  AUDIO_URL: "https://drive.google.com/file/d/1BPFNdE9-1nA9Xm-TNCbaKRRtAboD4XRX/view?usp=drive_link",
   
   // 📸 GANTI LINK FOTO PERTAMA DI SINI (BISA PAKAI LINK GOOGLE DRIVE)
-  FOTO_MAIN: "https://images.unsplash.com/photo-1551632811-561732d1e306?auto=format&fit=crop&w=400&q=80",
+  FOTO_MAIN: "https://drive.google.com/file/d/1fhAD1AqAY4vowxYeGim0QowYwXmuMMCg/view?usp=sharing",
   
   // 📸 GANTI LINK FOTO KEDUA (YANG ADA BINGKAI POLAROID) DI SINI (BISA PAKAI LINK GOOGLE DRIVE)
-  FOTO_GAME: "https://images.unsplash.com/photo-1469474968028-56623f02e42e?q=80&w=400"
+  FOTO_GAME: "https://drive.google.com/file/d/1fhAD1AqAY4vowxYeGim0QowYwXmuMMCg/view?usp=sharing"
 };
